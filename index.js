be62cdb48f635d431922ba3748898f25ba1a0132
kornphongP <kornphong.p@gmail.com>
2025-09-01 16:20:05 +0700

    feat : Add 2 code smells

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ function sayHello() {
 }
 sayHello();
 
-// ------------------------------
 // Code Smells / Warnings
-// 1. Unused variable
 const unusedVar = 42;
 
-// 2. Using var instead of let/const (considered legacy)
 var legacyVar = "I am old style";
 
-// 3. Function without proper return (SonarQube might warn if function could return something)
 function unusedFunction(a, b) {
-    return a + b; // but never called
+    return a + b;
+}
+
+function unusedFunction() {
+  let a = 1;
 }
